2c54a12711dafa1df96c6a6ddbcf67f92ace415d
Nicholas J. Weber <nicholas@nicholasjweber.com>
2023-03-30 11:08:53 -0500

    First UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './shared/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {}
+  constructor(public userService: UserService) {}
 }
